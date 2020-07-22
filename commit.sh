27edc1e6cafa888f152275da2c61d987000ba130
Chris Moore <0xCM00@gmail.com>
2020-07-22 14:31:13 -0500

    .

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
-git add -A &> git-add.log
-git commit -am "."
-git push
+git add -A | git-log.sh
+git commit -am "."  | git-log.sh
+git push | git-log.sh
 
 # git clone --progress XYZ &> git_clone.file
 # cd /k/z0/archives
