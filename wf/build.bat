61f1381a8409d4cbbc7ad5495adeab0bf2d86c78
Chris Moore <0xcm@gmail.com>
2020-11-06 13:59:31 -0600

    .

--- wf/build.bat
+++ wf/build.bat
@@ -7,10 +7,13 @@ echo %CmdSep% >> %CmdLog%
 set SlnPath="%ZDev%\z0.machine.sln"
 echo SlnPath:%SlnPath% >> %CmdLog%
 
-set BuildLog="%ZDb%\logs\build\z0.machine.binlog"
-echo BuildLog:%BuildLog% >> %CmdLog%
+set BinLog="%ZDb%\logs\build\z0.machine.binlog"
+echo BinLog:%BinLog% >> %CmdLog%
 
-set CmdExec=dotnet build %SlnPath% -c Custom -bl:%BuildLog%;ProjectImports=ZipFile -m -detailedSummary -graph:true
+::set CmdExec=dotnet build %SlnPath% -c Custom -bl:%BinLog%;ProjectImports=ZipFile -m -detailedSummary -graph:true
+set TextLog="%ZDb%\logs\build\z0.machine.log"
+
+set CmdExec=dotnet build %SlnPath% -c Custom -fl -flp:logfile=%TextLog%;verbosity=normal -m -graph:true
 echo CmdExec:%CmdExec% >> %CmdLog%
 
 echo on
