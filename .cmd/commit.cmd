5543455580ea6c94e8ab61fe2e00c5ad49764621
Chris Moore <0xCM00@gmail.com>
2020-12-09 18:39:54 -0600

    .

--- .cmd/commit.cmd
+++ .cmd/commit.cmd
@@ -3,6 +3,9 @@
 set GitLogPath=%ZDb%\logs\git\git-actions.log
 echo GitLogPath:%GitLogPath%
 
+set Sep=--------------------------------------------------------------------------------------
+echo %Sep% >> %GitLogPath%
+
 git add -A >> %GitLogPath%
 git commit -am "."  >> %GitLogPath%
 git push  >> %GitLogPath%
