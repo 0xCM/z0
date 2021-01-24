e81f7cdc63431e0c263c8f478cb9c42bfcf67dc8
Chris Moore <0xCM00@gmail.com>
2021-01-24 12:44:26 -0600

    .

---
+++ src/lang/src/languages/mysql/models/StorageTypes.cs
@@ -0,0 +1,107 @@
+//-----------------------------------------------------------------------------
+// Copyright   :  (c) Chris Moore, 2020
+// License     :  MIT
+//-----------------------------------------------------------------------------
+namespace Z0.Lang
+{
+    using static MySql.TypeAffinityKind;
+
+    public readonly partial struct MySql
+    {
+        public readonly partial struct StorageTypes
+        {
+            public readonly struct Null : IFixedStorageType<Null>
+            {
+                public string Name => "null";
+
+                public ByteSize Size => 0;
+
+                public TypeAffinityKind Affinity => None;
+            }
+
+            public readonly struct Int<T> : IFixedStorageType<T>
+            {
+                public string Name => "integer";
+
+                public ByteSize Size
+                    => memory.size<T>();
+
+                public TypeAffinityKind Affinity
+                    => Integer;
+            }
+
+            public readonly struct Int8 : IFixedStorageType<byte>
+            {
+                public string Name
+                    => default(Int<byte>).Name;
+
+                public ByteSize Size
+                    => default(Int<byte>).Size;
+
+                public TypeAffinityKind Affinity
+                    => default(Int<byte>).Affinity;
+            }
+
+            public readonly struct Int16 : IFixedStorageType<ushort>
+            {
+                public string Name
+                    => default(Int<ushort>).Name;
+
+                public ByteSize Size
+                    => default(Int<ushort>).Size;
+
+                public TypeAffinityKind Affinity
+                    => default(Int<ushort>).Affinity;
+            }
+
+            public readonly struct Int32 : IFixedStorageType<uint>
+            {
+                public string Name
+                    => default(Int<uint>).Name;
+
+                public ByteSize Size
+                    => default(Int<uint>).Size;
+
+                public TypeAffinityKind Affinity
+                    => default(Int<uint>).Affinity;
+            }
+
+            public readonly struct Int64 : IFixedStorageType<ulong>
+            {
+                public string Name
+                    => default(Int<ulong>).Name;
+
+                public ByteSize Size
+                    => default(Int<ulong>).Size;
+
+                public TypeAffinityKind Affinity
+                    => default(Int<ulong>).Affinity;
+            }
+
+            public readonly struct Real : IFixedStorageType<double>
+            {
+                public string Name => "real";
+
+                public ByteSize Size
+                    => memory.size<double>();
+
+                public TypeAffinityKind Affinity
+                    => TypeAffinityKind.Real;
+            }
+
+            public readonly struct SqlText : IStorageType<char[]>
+            {
+                public string Name => "text";
+
+                public TypeAffinityKind Affinity => Text;
+            }
+
+            public readonly struct SqlBlob : IStorageType<byte[]>
+            {
+                public string Name => "blob";
+
+                public TypeAffinityKind Affinity => Blob;
+            }
+        }
+    }
+}
