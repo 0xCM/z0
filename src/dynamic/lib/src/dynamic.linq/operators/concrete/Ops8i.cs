c25a46385e5a2c2c8864aa7c298f8b117468c8dc
Chris Moore <0xCM00@gmail.com>
2021-02-18 23:52:10 -0600

    .

--- src/dynamic/lib/src/dynamic.linq/operators/concrete/Ops8i.cs
+++ src/dynamic/lib/src/dynamic.linq/operators/concrete/Ops8i.cs
@@ -9,46 +9,49 @@ namespace Z0
 
     using static LinqXFunc;
 
-    public readonly struct Ops8i
+    partial struct DynamicOps
     {
-        public static Expression<Func<sbyte, sbyte, sbyte>> Add
-            => f<sbyte>((x, y) => (sbyte)(x + y));
 
-        public static Expression<Func<sbyte, sbyte, sbyte>> Mul
-            => f<sbyte>((x, y) => (sbyte)(x * y));
+        public readonly struct Ops8i
+        {
+            public static Expression<Func<sbyte, sbyte, sbyte>> Add
+                => f<sbyte>((x, y) => (sbyte)(x + y));
 
-        public static Expression<Func<sbyte, sbyte, sbyte>> Sub
-            => f<sbyte>((x, y) => (sbyte)(x - y));
+            public static Expression<Func<sbyte, sbyte, sbyte>> Mul
+                => f<sbyte>((x, y) => (sbyte)(x * y));
 
-        public static Expression<Func<sbyte,sbyte,sbyte>> And
-            => f<sbyte>((x, y) => (sbyte)(x & y));
+            public static Expression<Func<sbyte, sbyte, sbyte>> Sub
+                => f<sbyte>((x, y) => (sbyte)(x - y));
 
-        public static Expression<Func<sbyte,sbyte,sbyte>> Or
-            => f<sbyte>((x, y) => (sbyte)(x | y));
+            public static Expression<Func<sbyte,sbyte,sbyte>> And
+                => f<sbyte>((x, y) => (sbyte)(x & y));
 
-        public static Expression<Func<sbyte,sbyte,sbyte>> Xor
-            => f<sbyte>((x, y) => (sbyte)(x ^ y));
+            public static Expression<Func<sbyte,sbyte,sbyte>> Or
+                => f<sbyte>((x, y) => (sbyte)(x | y));
 
-        public static Expression<Func<sbyte, sbyte>> Abs
-            => f((sbyte x) => x < 0 ? (sbyte) -x : x);
+            public static Expression<Func<sbyte,sbyte,sbyte>> Xor
+                => f<sbyte>((x, y) => (sbyte)(x ^ y));
 
-        public static Expression<Func<sbyte, sbyte>> Inc
-            => f((sbyte x) => ++x);
+            public static Expression<Func<sbyte, sbyte>> Abs
+                => f((sbyte x) => x < 0 ? (sbyte) -x : x);
 
-        public static Expression<Func<sbyte, sbyte>> Dec
-            => f((sbyte x) => --x);
+            public static Expression<Func<sbyte, sbyte>> Inc
+                => f((sbyte x) => ++x);
 
-        public static Expression<Func<sbyte, sbyte, bool>> Gt
-            => f((sbyte x, sbyte y) => x > y);
+            public static Expression<Func<sbyte, sbyte>> Dec
+                => f((sbyte x) => --x);
 
-        public static Expression<Func<sbyte, sbyte, bool>> GtEq
-            => f((sbyte x, sbyte y) => x >= y);
+            public static Expression<Func<sbyte, sbyte, bool>> Gt
+                => f((sbyte x, sbyte y) => x > y);
 
-        public static Expression<Func<sbyte, sbyte, bool>> Lt
-            => f((sbyte x, sbyte y) => x < y);
+            public static Expression<Func<sbyte, sbyte, bool>> GtEq
+                => f((sbyte x, sbyte y) => x >= y);
 
-        public static Expression<Func<sbyte, sbyte, bool>> LtEq
-            => f((sbyte x, sbyte y) => x <= y);
+            public static Expression<Func<sbyte, sbyte, bool>> Lt
+                => f((sbyte x, sbyte y) => x < y);
+
+            public static Expression<Func<sbyte, sbyte, bool>> LtEq
+                => f((sbyte x, sbyte y) => x <= y);
+        }
     }
-
 }
